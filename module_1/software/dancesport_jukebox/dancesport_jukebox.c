74648a5ab59210ace36951db7dc90e1ba3a46dc5
Hirokuzu <arnold.hn.ip@gmail.com>
2015-10-15 19:38:47 -0700

    Add SD search.

--- module_1/software/dancesport_jukebox/dancesport_jukebox.c
+++ module_1/software/dancesport_jukebox/dancesport_jukebox.c
@@ -14,6 +14,7 @@
 #include "altera_up_avalon_video_pixel_buffer_dma.h"
 #include "dancesport_jukebox.h"
 #include "music.h"
+#include "system.h"
 
 int main() {
 	av_config_setup("/dev/audio_and_video_config_0");
@@ -27,74 +28,61 @@ int main() {
 		if (alt_up_sd_card_is_Present()) {
 			if (alt_up_sd_card_is_FAT16()) {
 				printf("FAT16\n");
-				int * list_len;
-				char ** playlist = get_genre_from_SD(root, list_len);
+				int list_len;
+				char ** playlist = get_genre_from_SD(root, &list_len);
 				int i = 0;
-				for(;i < *list_len; i++)
+				for(;i < list_len; i++)
 				{
-					printf("%i\n", *list_len);
 					printf("%s\n", playlist[i]);
 				}
-//				printf("FAT16\n");
-//				char * current_file = malloc(sizeof(char) * 13);
-//				if (alt_up_sd_card_find_first(root, current_file) == 0) {
-//					printf("File Found.\n");
-//					do {
-//						printf("%s\n", current_file);
-//						if(strcasestr(current_file, ".wav"))
-//						{
-//							printf("hey!\n");
-//							music song;
-//							song.filename = current_file;
-//							song.fd = alt_up_sd_card_fopen(current_file, false);
-//							// read the file
-//							if(song.fd != -1)
-//							{
-//								int count = 0;
-//								while(count < 40)
-//								{
-//									alt_up_sd_card_read(song.fd);
-//									count++;
-//								}
-//								printf("reading data size\n");
-//								int i;
-//								size_t data_size = 0;
-//								for(i = 0; i < 4; i++)
-//								{
-//									data_size = data_size | alt_up_sd_card_read(song.fd) << (i*8);
-//								}
-//								printf("File size: %i bytes.\n", data_size);
-//
-//								unsigned int left[data_size/4];
-//								unsigned int right[data_size/4];
-//								int j;
-//
-//								printf("Reading file...\n");
-//								for(i = 0; i < data_size/4; i++)
-//								{
-//									short int left_sample = alt_up_sd_card_read(song.fd);
-//									left_sample = alt_up_sd_card_read(song.fd) << 8 | left_sample;
-//									left[i] = left_sample;
-//									short int right_sample = alt_up_sd_card_read(song.fd);
-//									right_sample = alt_up_sd_card_read(song.fd) << 8 | right_sample;
-//									right[i] = right_sample;
-//								}
-//
-//								printf("Playing audio\n");
-//								for(i = 0; i < data_size/4; i++)
-//								{
-//									while(!alt_up_audio_write_fifo_space(audio_out,ALT_UP_AUDIO_LEFT));
-//									alt_up_audio_write_fifo(audio_out, &left[i], 1, ALT_UP_AUDIO_LEFT);
-//									alt_up_audio_write_fifo(audio_out, &right[i], 1, ALT_UP_AUDIO_RIGHT);
-//								}
-//							}
-//							else
-//							{
-//								printf("I can't read :(\nError reading %s.\n", song.filename);
-//							}
-//						}
-//					} while (alt_up_sd_card_find_next(current_file) == 0);
-//				}
+				music song;
+				song.filename = playlist[1];
+				song.fd = alt_up_sd_card_fopen(song.filename, false);
+				if(song.fd != -1)
+				{
+					int count = 0;
+					while(count < 40)
+					{
+						alt_up_sd_card_read(song.fd);
+						count++;
+					}
+					printf("reading data size\n");
+					int i;
+					size_t data_size = 0;
+					for(i = 0; i < 4; i++)
+					{
+						data_size = data_size | alt_up_sd_card_read(song.fd) << (i*8);
+					}
+					printf("File size: %i bytes.\n", data_size);
+
+					unsigned int left[data_size/4];
+					unsigned int right[data_size/4];
+					int j;
+
+					printf("Reading file...\n");
+					short int left_sample;
+					for(i = 0; i < data_size/4 && (left_sample = alt_up_sd_card_read(song.fd))!=-1 ; i++)
+					{
+						left_sample = alt_up_sd_card_read(song.fd) << 8 | left_sample;
+						left[i] = left_sample;
+						short int right_sample = alt_up_sd_card_read(song.fd);
+						right_sample = alt_up_sd_card_read(song.fd) << 8 | right_sample;
+						right[i] = right_sample;
+					}
+
+					printf("Playing audio\n");
+					alt_up_audio_enable_write_interrupt(audio_out);
+					for(i = 0; i < data_size/4; i++)
+					{
+						while(!alt_up_audio_write_fifo_space(audio_out,ALT_UP_AUDIO_LEFT));
+						alt_up_audio_write_fifo(audio_out, &left[i], 1, ALT_UP_AUDIO_LEFT);
+						alt_up_audio_write_fifo(audio_out, &right[i], 1, ALT_UP_AUDIO_RIGHT);
+					}
+				}
+				else
+				{
+					printf("I can't read :(\nError reading %s.\n", song.filename);
+				}
 			} else {
 				printf("SD card not FAT16. Exiting...\n");
 				return 3;
--- module_1/software/dancesport_jukebox/music.c
+++ module_1/software/dancesport_jukebox/music.c
@@ -23,34 +23,24 @@ char ** get_genre_from_SD(char * directory, int * list_len)
 	strcat(adjust_directory, ".");
 	if(alt_up_sd_card_find_first(adjust_directory, first_file) == 0) // directory actually exists, there's stuff here
 	{
-		printf("%s\n", first_file);
 		char ** playlist = malloc(sizeof(first_file) * playlist_max); // dunno if the first file is garbage and rest is good
 		playlist[0] = directory;
-		printf("Directory: %s\n", playlist[0]);
 		int count = 1;
 		playlist[count] = malloc(fat16_name_len);
 		strcpy(playlist[count],first_file);
 
 		do
 		{
-			printf("Playlist[%d]: %s\n", count, playlist[count]);
 			int namelen = strlen(playlist[count]);
 //			printf("namelen: %i\n", namelen);
-			printf("%s\n", *(playlist+count) + namelen - 4);
-			if(namelen > 4 && !strcmp(*(playlist+count) + namelen - 4, ".wav")) // actually a wav file
+			if(namelen > 4 && !strcasecmp((*(playlist+count)) + namelen - 4, ".wav")) // actually a wav file
 			{
 				count++; // we can move on
 			}
-		}while(alt_up_sd_card_find_next(playlist[count]) == 0);
-		printf("%d\n", count);
-		int i = 0;
-		for(i = 0; i < count; i++)
-		{
-			printf("%s\n", playlist[count]);
-		}
+		}while(alt_up_sd_card_find_next(*(playlist+count)) == 0);
 		if(count > 1)
 		{
-			* list_len = count;
+			*list_len = count;
 			return playlist;
 		}
 		else; // there were no useful files, just return a null pointer?

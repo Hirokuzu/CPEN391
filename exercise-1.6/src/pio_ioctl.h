783fa76e4fd5dc6ea4aef1dd7e3c83996e61d3c1
Hirokuzu <arnold.hn.ip@gmail.com>
2015-10-22 19:15:07 -0700

    Add exercise 1.6 PDF so setup for 1.6 is kept. Complete step 2 for exercise 1.6

--- exercise-1.6/src/pio_ioctl.h
+++ exercise-1.6/src/pio_ioctl.h
@@ -4,12 +4,19 @@
 
 #include <linux/ioctl.h>
 #include "hps_0.h"
+#include "socal/hps.h"
 
 #define MAJOR_NUM 122
 #define DEVICE_NAME "pio_ioctl"
 #define DEVICE_FILE_NAME "/dev/pio_ioctl"
 
+#define IOCTL_SET_LED _IOW(MAJOR_NUM, 0, char)
+#define IOCTL_GET_LED _IOR(MAJOR_NUM, 1, char)
+#define IOCTL_GET_DIPSW _IOR(MAJOR_NUM, 2, char)
+#define IOCTL_GET_BUTTON _IOR(MAJOR_NUM, 3, char)
 
+#define PIO_REGS_BASE (ALT_LWFPGASLVS_OFST + LED_PIO_BASE)
+#define PIO_REGS_SPAN (BUTTON_PIO)END - LED_PIO_BASE + 1)
 
 static int device_open(struct inode *inode, struct file *file);
 static int device_release(struct inode *inode, struct file *file);
